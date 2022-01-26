4d4c2ca57cc6862c3638aab9febee6ca88c91ade
Tanvir <tanvirmomin123@gmail.com>
2022-01-26 17:09:34 +0530

    heroku setup

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.use('/api/auth', authRoutes);
 // database connection
 connectDb;
 
+// deploy
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
